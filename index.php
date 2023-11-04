f4c38f3544a7026615ee208879ad9644bbfcdd85
DESKTOP-I3USAS7\PC <champoyradoo@gmail.com>
2023-11-04 19:15:37 +0800

    fix: copy

--- index.php
+++ index.php
@@ -16,12 +16,32 @@ if ($method != 'POST') {
 
 require_once "./vendor/autoload.php";
 
+function downloadFile($url, $path)
+{
+    $newfname = $path;
+    $file = fopen ($url, 'rb');
+    if ($file) {
+        $newf = fopen ($newfname, 'wb');
+        if ($newf) {
+            while(!feof($file)) {
+                fwrite($newf, fread($file, 1024 * 8), 1024 * 8);
+            }
+        }
+    }
+    if ($file) {
+        fclose($file);
+    }
+    if ($newf) {
+        fclose($newf);
+    }
+}
+
 $fileUrl = $_POST['file_url'];
 $pdfPath = "{$_SERVER['DOCUMENT_ROOT']}/temp/temp.pdf";
 $imagePath = "{$_SERVER['DOCUMENT_ROOT']}/temp/temp.png";
 
 
-copy($fileUrl, $pdfPath);
+downloadFile($fileUrl, $pdfPath);
 
 $pdf = new \Spatie\PdfToImage\Pdf($pdfPath);
 $pdf->saveImage($imagePath);
